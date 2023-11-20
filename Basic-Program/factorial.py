a0361f41ee3242195bc41cb1502069d21d77b058
Lokesh Kumar <lokeshkmr30697@gmail.com>
2023-11-20 11:32:51 +0530

    Basic-Program

---
+++ Basic-Program/factorial.py
@@ -0,0 +1,5 @@
+n=int(input("Enter any Number : "))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(f"Factorial of a Number {n} = {fact}")
---
+++ Basic-Program/powerOfNumber.py
@@ -0,0 +1,2 @@
+n=int(input("Enter any Number : "))
+print(f"Power of a Number {n} = {n**2}")
