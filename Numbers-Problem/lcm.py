83fd5334ab07f050e88973eb22e02bd80f1c2c67
Lokesh Kumar <lokeshkmr30697@gmail.com>
2023-11-22 10:43:43 +0530

    Number-Problem

--- Numbers-Problem/lcm.py
+++ Numbers-Problem/lcm.py
@@ -1,4 +1,20 @@
 def findLCM(n1,n2):
-    pass
+    # (formula lcm*hcf=product of two number)
+    res=findHCF(n1,n2)
+    lcm=n1*n2//res
+    return lcm
+    
+def findHCF(n1,n2):
+    mx=max(n1,n2)
+    hcf=1
+    for i in range(1,mx+1):
+        if n1%i==0 and n2%i==0:
+            hcf=i
+    return hcf
 
+
+n1=int(input("Enter First Number : "))
+n2=int(input("Enter Second Number : "))
+result=findLCM(n1,n2)
+print(f"LCM of Two Number {n1} and {n2} is {result}")
 
